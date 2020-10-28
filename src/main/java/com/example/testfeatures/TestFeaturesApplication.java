9cbaa70f2db88f7609053b7bdc1ebf3e7f2627c9
Igor Komissarov <igor.komissarov@elinvar.de>
2020-10-28 21:17:06 +0100

    Initial commit

---
+++ src/main/java/com/example/testfeatures/TestFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFeaturesApplication.class, args);
+	}
+
+}
